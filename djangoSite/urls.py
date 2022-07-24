bf5133b83c2713b75268f898e81a869dc910d00a
serg <serg.goncharenko.odessa@gmail.com>
2022-07-24 16:34:49 +0300

    Homework lesson 3 done

--- djangoSite/urls.py
+++ djangoSite/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import random
+import string
+
 from django.contrib import admin
 from django.urls import path, re_path
 from django.http import HttpRequest, HttpResponse, Http404
@@ -22,14 +25,26 @@ def home_page(request) -> HttpResponse:
     return HttpResponse("Main page")
 
 
+def article_page(request, article_id, article_slug) -> HttpResponse:
+    return HttpResponse(f"Article {article_id} <h1>{article_slug}</h1")
+
+
 def pass_page(request, password) -> HttpResponse:
-    return HttpResponse(f"trufk {password}")
+    letters = string.ascii_letters
+    digits = string.digits
+    if len(password) != 8:
+        return HttpResponse(f"Password not correct: must have 8 symbols")
+    for chr_in_pass in password:
+        if chr_in_pass not in letters and chr_in_pass not in digits:
+            return HttpResponse(f"Password not correct: may include only "
+                                f"small, capital latin symbols or digits ")
+    return HttpResponse(f"Password is correct")
 
 
-def pass_generate(request: HttpRequest, user_id) -> HttpResponse:
-    # if user_id in users:
-        return HttpResponse(f"user_id: {user_id}")
-
+def pass_generate(request, length) -> HttpResponse:
+    pass_symbols = string.ascii_letters + string.digits
+    password = random.sample(pass_symbols, length)
+    return HttpResponse(password)
 
 
 # /article/<article_id>/<article_slug> - буде повертати інформацію про статю: номер статі та ії залоговок (slug)
@@ -38,11 +53,12 @@ def pass_generate(request: HttpRequest, user_id) -> HttpResponse:
 # Виводи, чи відповідає переданий пароль цим параметрам, чи ні.
 # /password/generate/<length> - буде відображати випадково сгенерований пароль заданої довжини
 
+
 urlpatterns = [
     path('', home_page),
     path('home/', home_page),
     path('homepage/', home_page),
-    path('article/<article_id>/<article_slug>/', home_page),
-    path('password/<password>', pass_page),
-    path('/password/generate/<length> ', pass_generate)
+    path('article/<int:article_id>/<slug:article_slug>/', article_page),
+    path('password/<str:password>/', pass_page),
+    path('password/generate/<int:length>/', pass_generate),
 ]
